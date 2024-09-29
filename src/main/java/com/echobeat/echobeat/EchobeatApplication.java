d86b174385950c9781c38cd5e79257afdaf21f26
the-dg04 <devgup04@gmail.com>
2024-09-30 02:03:20 +0530

    init

---
+++ src/main/java/com/echobeat/echobeat/EchobeatApplication.java
@@ -0,0 +1,13 @@
+package com.echobeat.echobeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchobeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchobeatApplication.class, args);
+	}
+
+}
