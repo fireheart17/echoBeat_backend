60b182723f0c3747e8fbc4fc20954c4d4e356bf5
the-dg04 <devgup04@gmail.com>
2024-10-03 15:07:18 +0530

    added models

--- src/main/java/com/echobeat/model/models.sql
+++ src/main/java/com/echobeat/model/models.sql
@@ -121,3 +121,19 @@ CREATE TABLE IF NOT EXISTS SocialMedia (
     socialmediahandle VARCHAR(255),
     PRIMARY KEY (artist_id, socialmediatype)
 );
+
+CREATE TABLE IF NOT EXISTS followers (
+    artist_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY (artist_id, user_id)
+);
+CREATE TABLE IF NOT EXISTS liked_albums (
+    user_id BIGINT NOT NULL,
+    album_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, album_id)
+);
+CREATE TABLE IF NOT EXISTS liked_podcasts (
+    user_id BIGINT NOT NULL,
+    podcast_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, podcast_id)
+);
