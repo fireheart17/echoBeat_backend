1ff21c3fcfa2a9f39c7d11eb0759b5bbfaee26e3
fireheart17 <iampiyushgoyal17@gmail.com>
2024-11-05 03:06:20 +0530

    added liked albums

--- src/main/java/com/echobeat/controller/LikedAlbumsController.java
+++ src/main/java/com/echobeat/controller/LikedAlbumsController.java
@@ -1,11 +1,15 @@
 package com.echobeat.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.echobeat.model.LikedAlbums;
+import com.echobeat.model.Track;
+import com.echobeat.model.Album;
 import com.echobeat.repository.LikedAlbumsInterface;
 
 @CrossOrigin(origins = "http://localhost:8081")
@@ -33,6 +37,22 @@ public class LikedAlbumsController {
             return new ResponseEntity<>("Album unliked successfully.", HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Liked album not found.", HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @GetMapping("/likedAlbums/userId/{userId}")
+    public ResponseEntity<List<Album>> getLikedAlbumsByUserId(@PathVariable("userId") long userId) {
+        try {
+            List<Album> likedAlbums = likedAlbumsRepository.LikedAlbumsByUserId(userId);
+
+            if (likedAlbums.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(likedAlbums, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
