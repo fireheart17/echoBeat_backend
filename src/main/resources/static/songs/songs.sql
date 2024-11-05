db2cdfac4e4c0da19036fdc3851b1e3759940b0f
ShivanshK <shivansh.kandpal.cse22@itbhu.ac.in>
2024-11-05 19:06:00 +0530

    added songs.sql and songs in resources

---
+++ src/main/resources/static/songs/songs.sql
@@ -0,0 +1,33 @@
+-- CREATE TABLE IF NOT EXISTS tracks (
+--     track_id VARCHAR(255) PRIMARY KEY,
+--     track_name VARCHAR(255),
+--     genre VARCHAR(255),
+--     lyrics TEXT,
+--     duration INT,
+--     country VARCHAR(255),
+--     like_count INT DEFAULT 0,
+--     listen_count INT DEFAULT 0,
+--     album_id BIGINT DEFAULT 0,
+--     FOREIGN KEY (album_id) REFERENCES albums(album_id)
+--     ON DELETE SET NULL
+--     ON UPDATE CASCADE
+-- );
+INSERT into tracks values ("3EPXxR3ImUwfayaurPi3cm","Be Alright","Pop","A",3,"USA",0,0,0);
+INSERT into tracks values ("7qiZfU4dY1lWllzX7mPBI3","Shape of You","Pop","A",4,"USA",0,0,0);
+INSERT into tracks values ("6habFhsOp2NvshLv26DqMb","Despacito","Reggaeton","A",4,"USA",0,0,0);
+INSERT into tracks values ("3KkXRkHbMCARz0aVfEt68P","Sunflower","Hip-Hop","A",5,"USA",0,0,0);
+INSERT into tracks values ("0VjIjW4GlUZAMYd2vXMi3b","Blinding Lights","Synthwave","A",4,"USA",0,0,0);
+INSERT into tracks values ("5nujrmhLynf4yMoMtj8AQF","Levitating","Pop","A",3,"USA",0,0,0);
+INSERT into tracks values ("5QO79kh1waicV47BqGRL3g","Save Your Tears","Synthwave","A",4,"USA",0,0,0);
+INSERT into tracks values ("4iJyoBOLtHqaGxP12qzhQI","Peaches","R&B","A",3,"USA",0,0,0);
+INSERT into tracks values ("67BtfxlNbhBmCDR2L2l8qd","MONTERO (Call Me By Your Name)","Hip-Hop","A",3,"USA",0,0,0);
+INSERT into tracks values ("5HCyWlXZPP0y6Gqq8TgA20","STAY","Pop","A",2,"USA",0,0,0);
+INSERT into tracks values ("5CQ30WqJwcep0pYcV4AMNc","Stairway to Heaven","Rock","A",8,"USA",0,0,0);
+INSERT into tracks values ("3hRV0jL3vUpRrcy398teAU","The Night We Met","Indie","A",3,"USA",0,0,0);
+INSERT into tracks values ("6DCZcSspjsKoFjzjrWoCdn","God's Plan","Hip-Hop","A",3,"USA",0,0,0);
+INSERT into tracks values ("4nVBt6MZDDP6tRVdQTgxJg","Story of My Life","Pop","A",4,"USA",0,0,0);
+INSERT into tracks values ("2tpWsVSb9UEmDRxAl1zhX1","Counting Stars","Pop","A",4,"USA",0,0,0);
+INSERT into tracks values ("35RWttZt8zGGzT0mWtlXpn","Jab Tak","Bollywood","A",4,"India",0,0,0);
+INSERT into tracks values ("6DXLO8LndZMVOHM0wNbpzg","Danza Kuduro","Reggaeton","A",4,"Puerto Rico",0,0,0);
+INSERT into tracks values ("7JJmb5XwzOO8jgpou264Ml","There's Nothing Holdin' Me Back","Pop","A",3,"Canada",0,0,0);
+INSERT into tracks values ("4mBmsPcPa1Eu4LDTHq55Ab","Hothon Se Chhu Lo Tum","Bollywood","A",5,"India",0,0,0);
