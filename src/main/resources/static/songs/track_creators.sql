bc1d719003b3e7e469e740729ba1bc05f85ed50c
abhinavm13 <abhinavmittal132@gmail.com>
2024-11-05 23:19:56 +0530

    Added Artists and track_creators data

---
+++ src/main/resources/static/songs/track_creators.sql
@@ -0,0 +1,30 @@
+-- CREATE TABLE IF NOT EXISTS track_creators (
+--     track_id VARCHAR(255),
+--     artist_id BIGINT,
+--     PRIMARY KEY (track_id, artist_id),
+--     FOREIGN KEY (track_id) REFERENCES tracks(track_id)
+--     ON DELETE CASCADE
+--     ON UPDATE CASCADE,
+--     FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+--     ON DELETE CASCADE
+--     ON UPDATE CASCADE
+-- );
+INSERT into artists values ("EPXxR3ImUwfayaurPi3cm",1);
+INSERT into artists values ("qiZfU4dY1lWllzX7mPBI3",2);
+INSERT into artists values ("6habFhsOp2NvshLv26DqMb",3);
+INSERT into artists values ("3KkXRkHbMCARz0aVfEt68P",4);
+INSERT into artists values ("VjIjW4GlUZAMYd2vXMi3b",5);
+INSERT into artists values ("5nujrmhLynf4yMoMtj8AQF",6);
+INSERT into artists values ("5QO79kh1waicV47BqGRL3g",7);
+INSERT into artists values ("4iJyoBOLtHqaGxP12qzhQI",8);
+INSERT into artists values ("67BtfxlNbhBmCDR2L2l8qd",9);
+INSERT into artists values ("5HCyWlXZPP0y6Gqq8TgA20",10);
+INSERT into artists values ("5CQ30WqJwcep0pYcV4AMNc",5);
+INSERT into artists values ("3hRV0jL3vUpRrcy398teAU",8);
+INSERT into artists values ("6DCZcSspjsKoFjzjrWoCdn",3);
+INSERT into artists values ("4nVBt6MZDDP6tRVdQTgxJg",6);
+INSERT into artists values ("2tpWsVSb9UEmDRxAl1zhX1",7);
+INSERT into artists values ("35RWttZt8zGGzT0mWtlXpn",5);
+INSERT into artists values ("6DXLO8LndZMVOHM0wNbpzg",8);
+INSERT into artists values ("7JJmb5XwzOO8jgpou264Ml",2);
+INSERT into artists values ("4mBmsPcPa1Eu4LDTHq55Ab",7);
