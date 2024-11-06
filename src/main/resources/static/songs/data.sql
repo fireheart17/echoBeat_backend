8744076ec96fd8db774e04b4d25bf969416a67d5
the-dg04 <devgup04@gmail.com>
2024-11-07 02:14:58 +0530

    fix

--- src/main/resources/static/songs/data.sql
+++ src/main/resources/static/songs/data.sql
@@ -1,3 +1,340 @@
+CREATE TABLE IF NOT EXISTS subscriptions (
+    subscription_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    price INT,
+    duration INT
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+    album_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    genre VARCHAR(255),
+    release_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS charts (
+    chart_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    chart_type VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS podcast (
+    podcast_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    podcast_name VARCHAR(255),
+    genre VARCHAR(100),
+    lyrics TEXT,
+    duration INT,
+    country VARCHAR(100),
+    like_count INT DEFAULT 0,
+    listen_count INT DEFAULT 0
+);
+
+
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    artist_name VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    password VARCHAR(255),
+    global_rank VARCHAR(50),
+    country VARCHAR(100),
+    about TEXT,
+    follower_count INT DEFAULT 0
+);
+
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    username VARCHAR(255),
+    password VARCHAR(255),
+    dob DATE,
+    age INT,
+    gender VARCHAR(10),
+    subscription_id BIGINT,
+    subscription_end_date VARCHAR(255),
+    FOREIGN KEY (subscription_id) REFERENCES subscriptions(subscription_id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS playlists (
+    playlist_id BIGINT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) ,
+    duration INT ,
+    user_id BIGINT ,
+    PRIMARY KEY (playlist_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS tracks (
+    track_id VARCHAR(255) PRIMARY KEY,
+    track_name VARCHAR(255),
+    genre VARCHAR(255),
+    lyrics TEXT,
+    duration INT,
+    country VARCHAR(255),
+    like_count INT DEFAULT 0,
+    listen_count INT DEFAULT 0,
+    album_id BIGINT DEFAULT 0,
+    FOREIGN KEY (album_id) REFERENCES albums(album_id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS track_creators (
+    track_id VARCHAR(255),
+    artist_id BIGINT,
+    PRIMARY KEY (track_id, artist_id),
+    FOREIGN KEY (track_id) REFERENCES tracks(track_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS liked_playlists (
+    user_id BIGINT,
+    playlist_id BIGINT,
+    PRIMARY KEY (user_id, playlist_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS liked_songs (
+    user_id BIGINT NOT NULL,
+    track_id VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id, track_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (track_id) REFERENCES tracks(track_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS perks (
+    subscription_id BIGINT NOT NULL,
+    description VARCHAR(255),
+    PRIMARY KEY (subscription_id,description),
+    FOREIGN KEY (subscription_id) REFERENCES subscriptions(subscription_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS playlist_tracks (
+    playlist_id BIGINT NOT NULL,
+    track_id VARCHAR(255) NOT NULL,
+    PRIMARY KEY (playlist_id, track_id),
+    FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (track_id) REFERENCES tracks(track_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS podcast_creators (
+    podcast_id BIGINT,
+    artist_id BIGINT,
+    PRIMARY KEY (podcast_id, artist_id),
+    FOREIGN KEY (podcast_id) REFERENCES podcast(podcast_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS rankings (
+    track_id VARCHAR(255),
+    chart_id BIGINT,
+    rank_val BIGINT,
+    PRIMARY KEY (chart_id, rank_val),
+    FOREIGN KEY (track_id) REFERENCES tracks(track_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (chart_id) REFERENCES charts(chart_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS SocialMedia (
+    artist_id BIGINT,
+    socialmediatype VARCHAR(255),
+    socialmediahandle VARCHAR(255),
+    PRIMARY KEY (artist_id, socialmediatype),
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS followers (
+    artist_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    PRIMARY KEY (artist_id, user_id),
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS liked_albums (
+    user_id BIGINT NOT NULL,
+    album_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, album_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (album_id) REFERENCES albums(album_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS liked_podcasts (
+    user_id BIGINT NOT NULL,
+    podcast_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, podcast_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (podcast_id) REFERENCES podcast(podcast_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS created_album (
+    album_id BIGINT NOT NULL,
+    artist_id BIGINT NOT NULL,
+    PRIMARY KEY (album_id, artist_id),
+    FOREIGN KEY (album_id) REFERENCES albums(album_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+DROP TRIGGER IF EXISTS update_follower_count_after_insert;
+DROP TRIGGER IF EXISTS update_follower_count_after_delete;
+DROP TRIGGER IF EXISTS increment_like_count_for_tracks;
+DROP TRIGGER IF EXISTS decrement_like_count_for_tracks;
+DROP TRIGGER IF EXISTS increment_like_count_for_podcasts;
+DROP TRIGGER IF EXISTS decrement_like_count_for_podcasts;
+
+DELIMITER //
+
+CREATE TRIGGER update_follower_count_after_insert
+AFTER INSERT ON followers
+FOR EACH ROW
+BEGIN
+    UPDATE artists
+    SET follower_count = follower_count + 1
+    WHERE artist_id = NEW.artist_id;
+END //
+
+DELIMITER ;
+
+
+
+
+DELIMITER //
+
+
+CREATE TRIGGER update_follower_count_after_delete
+AFTER DELETE ON followers
+FOR EACH ROW
+BEGIN
+    UPDATE artists
+    SET follower_count = GREATEST(follower_count - 1, 0)
+    WHERE artist_id = OLD.artist_id;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+
+CREATE TRIGGER increment_like_count_for_tracks
+AFTER INSERT ON liked_songs
+FOR EACH ROW
+BEGIN
+    UPDATE tracks
+    SET like_count = like_count + 1
+    WHERE track_id = NEW.track_id;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+
+CREATE TRIGGER decrement_like_count_for_tracks
+AFTER DELETE ON liked_songs
+FOR EACH ROW
+BEGIN
+    UPDATE tracks
+    SET like_count = GREATEST(like_count - 1, 0)
+    WHERE track_id = OLD.track_id;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+
+CREATE TRIGGER increment_like_count_for_podcasts
+AFTER INSERT ON liked_podcasts
+FOR EACH ROW
+BEGIN
+    UPDATE podcast
+    SET like_count = like_count + 1
+    WHERE podcast_id = NEW.podcast_id;
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+
+CREATE TRIGGER decrement_like_count_for_podcasts
+AFTER DELETE ON liked_podcasts
+FOR EACH ROW
+BEGIN
+    UPDATE podcast
+    SET like_count = GREATEST(like_count - 1, 0)
+    WHERE podcast_id = OLD.podcast_id;
+END //
+
+DELIMITER ;
+
+
+
+
+
 -- CREATE TABLE IF NOT EXISTS subscriptions (
 --     subscription_id BIGINT AUTO_INCREMENT PRIMARY KEY,
 --     title VARCHAR(255),
