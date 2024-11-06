43184fe8d0342bf3aba6fd78cb41f62dc910a483
Abhigyan003 <abhigyan.official88@gmail.com>
2024-11-06 23:35:26 +0530

    added delete from liked songs

--- src/main/java/com/echobeat/controller/LikedSongsController.java
+++ src/main/java/com/echobeat/controller/LikedSongsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,16 @@ public class LikedSongsController {
         try {
             likedSongsRepository.save(likedSongs);
             return new ResponseEntity<>("Liked song saved successfully.", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/likedSongs/{userId}/{trackId}")
+    public ResponseEntity<String> deleteLikedSong(@PathVariable("userId") long userId, @PathVariable("trackId") String trackId) {
+        try {
+            likedSongsRepository.delete(userId, trackId);
+            return new ResponseEntity<>("Liked song deleted successfully.", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
